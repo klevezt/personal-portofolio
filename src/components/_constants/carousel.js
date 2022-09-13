2175e45f2b67f1b5a7168b88b7574f1f8b6bad18
klevezt <klevestpalucaj@gmail.com>
2022-09-13 16:38:42 +0300

    commit

--- src/components/_constants/carousel.js
+++ src/components/_constants/carousel.js
@@ -1,61 +1,61 @@
 export const heroCarouselItems = [
   {
     label: "React",
-    imgPath: "logo.svg",
+    imgPath: "/logo.svg",
     description:
       "React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”.",
   },
   {
     label: "NodeJS",
-    imgPath: "nodejs.svg",
+    imgPath: "/nodejs.svg",
     description:
       "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.",
   },
   {
     label: "MongoDB",
-    imgPath: "mongodb.svg",
+    imgPath: "/mongodb.svg",
     description:
       "MongoDB is a document database with the scalability and flexibility that you want with the querying and indexing that you need.",
   },
   {
     label: "Tailwind",
-    imgPath: "tailwind-css-2.svg",
+    imgPath: "/tailwind-css-2.svg",
     description:
       "Tailwind CSS makes it quicker to write and maintain the code of your application by using utility classes to control the padding, margin, color, font, and more of your application.",
   },
   {
     label: "Bootstrap 5",
-    imgPath: "bootstrap5.svg",
+    imgPath: "/bootstrap5.svg",
     description: "Bootstrap is a powerful, feature-packed frontend toolkit.",
   },
   {
     label: "Material UI",
-    imgPath: "materialui.svg",
+    imgPath: "/materialui.svg",
     description:
       "MUI offers a comprehensive suite of UI tools to help you ship new features faster.",
   },
   {
     label: "Firebase",
-    imgPath: "firebase.svg",
+    imgPath: "/firebase.svg",
     description:
       "Firebase is an app development platform that helps you build and grow apps.",
   },
   {
     label: "Vite",
-    imgPath: "vite.svg",
+    imgPath: "/vite.svg",
     description:
       "Vite is a build tool that aims to provide a faster and leaner development experience for modern web projects.",
   },
   {
     label: "Heroku",
-    imgPath: "heroku.svg",
+    imgPath: "/heroku.svg",
     description:
       "Heroku is a platform as a service based on a managed container system for deploying and running modern apps.",
   },
 
   // {
   //   label: "Redux",
-  //   imgPath: "redux.svg",
+  //   imgPath: "/redux.svg",
   //   description:
   //     "Redux is a predictable state container for JavaScript apps. It helps you write applications that behave consistently, run in different environments (client, server, and native), and are easy to test.",
   // },
