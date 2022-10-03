244caccd2bb41e39a3a1f3e90f4d267f8f2fd87c
klevezt <klevestpalucaj@gmail.com>
2022-10-03 14:45:43 +0300

    commit

---
+++ src/components/_ads/Adsense.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Adsense = () => {
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{ display: "inline-block", width: "728px", height: "90px" }}
+      data-ad-client="ca-pub-4210779179353314"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+  );
+}
+
+export default Adsense
